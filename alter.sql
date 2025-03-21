3bfd45153b40973df8633df4f95faae062ff7aa2
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-03-21 17:36:04 +0600

    first commit

---
+++ alter.sql
@@ -0,0 +1,19 @@
+SELECT * FROM person;
+
+ALTER TABLE person ADD COLUMN email VARCHAR(25) DEFAULT 'default@gmail.com' NOT NULL;
+-- inster a value into table
+INSERT INTO person VALUES(7,'shakil',45,'rimon@gmail.com');
+
+-- Add Column
+ALTER TABLE person DROP COLUMN email;
+
+-- Rename column
+ALTER TABLE person RENAME COLUMN age to user_age;
+-- Column type or const change
+ALTER TABLE person ALTER COLUMN user_name TYPE VARCHAR(50);
+
+-- Column set cont
+ALTER TABLE person ALTER COLUMN user_age SET NOT NULL;
+
+-- remove const
+ALTER TABLE person ALTER COLUMN user_age DROP NOT NULL;
