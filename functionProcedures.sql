2147c55940f5707322d8aee274a0220e7f2f4cce
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-03-23 20:21:34 +0600

    add procudure function

--- functionProcedures.sql
+++ functionProcedures.sql
@@ -30,3 +30,32 @@ SELECT delete_emp();
 SELECT emp_count();
 
 SELECT * FROM employess;
+
+
+CREATE PROCEDURE remove_emp_var()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+test_var INT;
+BEGIN
+    SELECT employee_id INTO test_var FROM employess WHERE employee_id = 25;
+    DELETE FROM employess WHERE employee_id = test_var;
+END;
+$$;
+
+call remove_emp_var();
+
+
+CREATE PROCEDURE remove_emp_var_by_id(p_emp_id INT)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+test_var INT;
+BEGIN
+    SELECT employee_id INTO test_var FROM employess WHERE employee_id = p_emp_id;
+    DELETE FROM employess WHERE employee_id = test_var;
+    RAISE NOTICE 'Employee Remove Successfully!';
+END;
+$$;
+
+call remove_emp_var_by_id(2);
