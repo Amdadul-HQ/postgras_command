864be1c74783999efb5ae0c0805103ac18aaacaa
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-03-23 20:03:07 +0600

    procedures sql

--- functionProcedures.sql
+++ functionProcedures.sql
@@ -7,5 +7,26 @@ $$
     SELECT count(*) FROM employess;
 $$;
 
+CREATE or REPLACE FUNCTION delete_emp()
+RETURNS void
+LANGUAGE SQL
+AS
+$$
+    DELETE FROM employess WHERE employee_id = 30;
+$$;
+
+CREATE or REPLACE FUNCTION delete_emp_by_id(p_emp_id INT)
+RETURNS void
+LANGUAGE SQL
+AS
+$$
+    DELETE FROM employess WHERE employee_id = p_emp_id;
+$$;
+
+SELECT delete_emp_by_id(1);
+
+SELECT delete_emp();
 
 SELECT emp_count();
+
+SELECT * FROM employess;
